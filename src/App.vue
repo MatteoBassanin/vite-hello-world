2924f60a400098fb5142c6a21b1d21a633fcc836
MatteoBassanin <matteo.bassanin@gmail.com>
2023-03-25 10:59:48 +0100

    added img

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script>
+import jumbotron from './components/jumbotron.vue';
+
+export default {
+
+
+  components: {
+    jumbotron
+  }
+
+}
+
+
 
 </script>
 
 <template>
   <h1>La mia prima app con Vite!</h1>
+  <jumbotron />
 </template>
 
 <style scoped>
---
+++ src/components/jumbotron.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="text_center">
+        <img src="https://giornaledeinavigli.it/media/2018/11/Giornata-mondiale-del-gatto-nero.jpg" alt="gatto nero">
+    </div>
+</template>
+
+
+
+<style>
+img {
+    margin: auto;
+}
+</style>
+
+<script>
+
+</script>
